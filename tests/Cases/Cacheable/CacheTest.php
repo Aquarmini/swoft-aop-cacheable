70efc836b711be8e1a453d9d3b7e32e1dfe8354e
李铭昕 <715557344@qq.com>
2018-07-24 11:55:53 +0800

    删除无用代码

---
+++ tests/Cases/Cacheable/CacheTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Swoft Entity Cache
+ *
+ * @author   limx <715557344@qq.com>
+ * @link     https://github.com/limingxinleo/swoft-aop-cacheable
+ */
+
+namespace SwoftTest\Db\Cases\Cacheable;
+
+use SwoftTest\Db\Cases\AbstractMysqlCase;
+use SwoftTest\Db\Testing\Entity\User;
+use Swoftx\Db\Entity\Config\ModelCacheConfig;
+
+class CacheTest extends AbstractMysqlCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
